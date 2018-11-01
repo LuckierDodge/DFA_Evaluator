95dcab3d7b88fa4e327bcd466a75d58a7b9fbc5f
Ryan Lewis <ryandlewis.rl@gmail.com>
2018-11-01 17:59:17 -0500

    Added word evaluation

--- dfa.cpp
+++ dfa.cpp
@@ -52,12 +52,26 @@ int main() {
 	for (int i = 0; i < nstates; i++) {
 		bool accepts = false;
 
-		for (int j = 0; j < naccepting; j++) {
-			accepts = (accepting[j] == i ? true : false);
-		}
+		for (int j = 0; j < naccepting; j++)
+			accepts = (accepting[j] == i ? true : accepts);
 		
 		cout << (accepts ? "* " : "  ") << i << " | " << transitions[i][0] << " " << transitions[i][1] << endl;
 	}
 
 	// Process Words
+	for (int w = 0; w < nwords; w++) {
+		string word = words[w];
+		int state = 0;
+		for ( string::iterator it=word.begin(); it != word.end(); ++it) {
+			if (*it == 'a')
+				state = transitions[state][0];
+			else
+				state = transitions[state][1];
+		}
+
+		bool accepts = false;
+		for (int a = 0; a < naccepting; a++)
+			accepts = (accepting[a] == state ? true: accepts);
+		cout << word << (accepts ? " accepted" : " rejected") << endl;
+	}
 }
