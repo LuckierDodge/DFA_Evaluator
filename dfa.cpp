170bbc70c314cb2afcddab506f30527cffebe79d
Ryan Lewis <ryandlewis.rl@gmail.com>
2018-11-01 18:02:12 -0500

    Fixed formatting

--- dfa.cpp
+++ dfa.cpp
@@ -47,7 +47,7 @@ int main() {
 
 	// Print Table
 	cout << "    | a b" << endl;
-	cout << "----+-----" << endl;
+	cout << "----+------" << endl;
 
 	for (int i = 0; i < nstates; i++) {
 		bool accepts = false;
