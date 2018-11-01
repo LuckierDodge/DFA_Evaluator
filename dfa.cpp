58bb5e15d0dddce8ccdabc764366f182b7ce001e
Ryan Lewis <ryandlewis.rl@gmail.com>
2018-11-01 17:36:21 -0500

    Input and table printing working, included example inputs

--- dfa.cpp
+++ dfa.cpp
@@ -4,12 +4,60 @@
 
 using namespace std;
 
+
 int main() {
-//	string buffer1 = "";
-//	string buffer2 = "";
-//	cin >> buffer1;// >> buffer2;
-//	int nstates = stoi(buffer1, nullptr);
-//	//int naccepting = stoi(buffer2, nullptr);
-//	cout << nstates;
-//	//cout << buffer2;
+	//Read from File
+
+	string buffer1;
+	string buffer2;
+	cin >> buffer1 >> buffer2;
+	int nstates = stoi(buffer1, nullptr);
+	int naccepting = stoi(buffer2, nullptr);
+	
+	buffer1 = "";
+	buffer2 = "";
+
+	int accepting[naccepting];
+
+	for (int i = 0; i < naccepting; i++) {
+		cin >> buffer1;
+		accepting[i] = stoi(buffer1, nullptr);
+	}
+
+	buffer1 = "";
+	int transitions[nstates][2];
+
+	for (int j = 0; j < nstates; j++) {
+		cin >> buffer1 >> buffer2;
+		transitions[j][0] = stoi(buffer1, nullptr);
+		transitions[j][1] = stoi(buffer2, nullptr);
+	}
+
+	buffer1 = "";
+	int nwords;
+	cin >> buffer1;
+	nwords = stoi(buffer1, nullptr);
+
+	string words[nwords];
+
+	for (int k = 0; k < nwords; k++) {
+		cin >> words[k];
+	}
+
+
+	// Print Table
+	cout << "    | a b" << endl;
+	cout << "----+-----" << endl;
+
+	for (int i = 0; i < nstates; i++) {
+		bool accepts = false;
+
+		for (int j = 0; j < naccepting; j++) {
+			accepts = (accepting[j] == i ? true : false);
+		}
+		
+		cout << (accepts ? "* " : "  ") << i << " | " << transitions[i][0] << " " << transitions[i][1] << endl;
+	}
+
+	// Process Words
 }
